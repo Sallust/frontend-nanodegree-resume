86fa6f47286c9dc6957cc443c451d56f239abca9
Sallust <daniyom@gmail.com>
2015-08-03 13:12:42 -0400

    Starting to mess around w/ JavaScript. Print to console and using replace function

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,23 @@
+$("#main").append("Danny Haile");
+
+var firstName = "Danny";
+
+console.log(firstName);
+
+var awesomeThoughts = "I am pretty awesome! Howe awesome you wonder?";
+
+console.log(awesomeThoughts);
+
+console.log("Mind Blown!");
+
+var email = "daniyom@gmail.com";
+
+console.log(email.replace("gmail","awesome"));
+
+var funThoughts = awesomeThoughts.replace("awesome","fuckin awesome");
+
+console.log(funThoughts)
+
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
