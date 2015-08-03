7366fb9e433e7e98b4e048ed2a02c68684f64e2a
Sallust <daniyom@gmail.com>
2015-08-03 18:25:37 -0400

    Use js to create var role, name formattedName, and formattedRole. Use js to prepend vars into header after running .replace on "%data%"

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,9 +2,12 @@ $("#main").append("Danny Haile");
 
 var firstName = "Danny";
 
+var name = "Danny Haile"
+var role = "Front-End Web Developer"
+
 console.log(firstName);
 
-var awesomeThoughts = "I am pretty awesome! Howe awesome you wonder?";
+var awesomeThoughts = "I am pretty awesome! How awesome you wonder?";
 
 console.log(awesomeThoughts);
 
@@ -17,6 +20,18 @@ console.log(email.replace("gmail","awesome"));
 var funThoughts = awesomeThoughts.replace("awesome","fuckin awesome");
 
 console.log(funThoughts)
+
+$("#main").append(funThoughts);
+
+var formattedName = HTMLheaderName.replace("%data%",name);
+
+var formattedRole = HTMLheaderRole.replace("%data%",role);
+
+$("#header").prepend(formattedRole);
+
+$("#header").prepend(formattedName);
+
+
 
 /*
 This is empty on purpose! Your code to build the resume will go here.
