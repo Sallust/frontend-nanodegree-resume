aeaa6e592623c51374417a8c6e595ae7d28e98d5
Sallust <daniyom@gmail.com>
2015-08-16 15:08:36 -0400

    Rearrange code for readability. Group JSON, then display functions, and then the calling of functions

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,8 +1,3 @@
-
-
-
-//var skills = ["awesomness","amazingness","JS","HTML","CSS"]
-
 var bio = {
 	"name" : "Danny Haile",
 	"role" : "Front-End Ninja in Training",
@@ -16,39 +11,7 @@ var bio = {
 	"welcome" : "Welcome to the awesomness that is my resume",
 	"pictureURL" : "images/me.jpg",
 	"skills" : ["awesomness","amazingness","JS","HTML","CSS"]
- };
-
-
-
-
-
-
-bio.display = function() {
-	var formattedName = HTMLheaderName.replace("%data%",bio.name);
-	var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
-	var formattedPicture = HTMLbioPic.replace("%data%",bio.pictureURL);
-	var formattedWelcome = HTMLwelcomeMsg.replace("%data%",bio.welcome);
-
-	for (contact in bio.contacts) {
-		window['formatted' + capital(contact)] = HTMLcontactGeneric.replace("%contact%",contact).replace("%data%",bio.contacts[contact]);
-	}
-
-	$("#header").prepend(formattedName,formattedRole);
-	$("#header").append(formattedPicture,formattedWelcome);
-
-	$("#topContacts").append(formattedMobile,formattedEmail,formattedGithub);
-	$("#footerContacts").append(formattedMobile,formattedTwitter)
-
-	if (bio.skills) {
-		$("#header").append(HTMLskillsStart);
-	}
-	for (skill in bio.skills) {
-		var formattedSkill = HTMLskills.replace("%data%",bio.skills[skill])
-		$("#skills").prepend(formattedSkill)
-	}
-}
-
-bio.display();
+ }
 
 var work = {
 	"jobs": [
@@ -68,20 +31,6 @@ var work = {
 		}
 	]
 }
-work.display = function() {
-	for (job in work.jobs) {
-		$("#workExperience").append(HTMLworkStart);
-		var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-		var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-		var formattedDate = HTMLworkDates.replace("%data%", work.jobs[job].dates);
-		var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
-		var formattedworkDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
-
-		$(".work-entry:last").append(formattedEmployer+formattedTitle+formattedDate+formattedLocation+formattedworkDescription);
-	}
-}
-
-work.display();
 
 var projects = {
 	"project": [
@@ -105,23 +54,6 @@ var projects = {
 		}
 	]
 }
-
-projects.display = function() {
-	for (project in projects.project) {
-		$("#projects").append(HTMLprojectStart);
-		var formattedTitle = HTMLprojectTitle.replace("%data%", projects.project[project].title);
-		var formattedDate = HTMLprojectDates.replace("%data%", projects.project[project].dates);
-		var formattedDescription = HTMLprojectDescription.replace("%data%", projects.project[project].description);
-		var formattedImages = ""
-		for (image in projects.project[project].images) {
-			var formattedImage = HTMLprojectImage.replace("%data%", projects.project[project].images[image]);
-			formattedImages = formattedImages + formattedImage;
-		}
-		$(".project-entry:last").append(formattedTitle,formattedDate,formattedDescription,formattedImages);
-	}
-}
-projects.display();
-
 
 var education = {
     "schools": [
@@ -163,6 +95,60 @@ var education = {
     	}
     ]
 }
+
+bio.display = function() {
+	var formattedName = HTMLheaderName.replace("%data%",bio.name);
+	var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
+	var formattedPicture = HTMLbioPic.replace("%data%",bio.pictureURL);
+	var formattedWelcome = HTMLwelcomeMsg.replace("%data%",bio.welcome);
+
+	for (contact in bio.contacts) {
+		window['formatted' + capital(contact)] = HTMLcontactGeneric.replace("%contact%",contact).replace("%data%",bio.contacts[contact]);
+	}  //Creation of formattedContact variables using HTMLcontactGeneric
+
+	$("#header").prepend(formattedName,formattedRole);
+	$("#header").append(formattedPicture,formattedWelcome);
+
+	$("#topContacts").append(formattedMobile,formattedEmail,formattedGithub);
+	$("#footerContacts").append(formattedMobile,formattedTwitter)
+
+	if (bio.skills) {
+		$("#header").append(HTMLskillsStart);
+	}
+	for (skill in bio.skills) {
+		var formattedSkill = HTMLskills.replace("%data%",bio.skills[skill])
+		$("#skills").prepend(formattedSkill)
+	}
+}
+
+work.display = function() {
+	for (job in work.jobs) {
+		$("#workExperience").append(HTMLworkStart);
+		var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+		var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+		var formattedDate = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+		var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+		var formattedworkDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+
+		$(".work-entry:last").append(formattedEmployer+formattedTitle+formattedDate+formattedLocation+formattedworkDescription);
+	}
+}
+
+projects.display = function() {
+	for (project in projects.project) {
+		$("#projects").append(HTMLprojectStart);
+		var formattedTitle = HTMLprojectTitle.replace("%data%", projects.project[project].title);
+		var formattedDate = HTMLprojectDates.replace("%data%", projects.project[project].dates);
+		var formattedDescription = HTMLprojectDescription.replace("%data%", projects.project[project].description);
+		var formattedImages = ""
+		for (image in projects.project[project].images) {
+			var formattedImage = HTMLprojectImage.replace("%data%", projects.project[project].images[image]);
+			formattedImages = formattedImages + formattedImage;
+		}
+		$(".project-entry:last").append(formattedTitle,formattedDate,formattedDescription,formattedImages);
+	}
+}
+
 education.display = function() {
 
 	for (school in education.schools) {
@@ -185,27 +171,23 @@ education.display = function() {
 	}
 }
 
-
+bio.display();
+work.display();
+projects.display();
 education.display();
+
 
 $(document).click(function(loc) {
 	logClicks(loc.pageX, loc.pageY);
-
 });
 
 $("#main").append(internationalizeButton)
 
 function inName() {
 	names = name.split(" ");
-
-
     names[1] = names[1].toUpperCase();
     names[0] = names[0][0].toUpperCase() + names[0].slice(1).toLowerCase();
-
-
-
 	return names[0] + " " + names[1]
-
 }
 
 $("#mapDiv").append(googleMap)
